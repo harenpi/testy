c3ef7f0300636153acf65660ee72d3736f87db20
Piotr <harenpi_aehit@students.vizja.pl>
2026-06-05 18:55:12 +0200

    Fix Npgsql DateTime timezone issue

--- src/ITServiceHelpDesk.Web/Program.cs
+++ src/ITServiceHelpDesk.Web/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
